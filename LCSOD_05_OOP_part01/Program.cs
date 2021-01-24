6074be646f2d6320a1aec5f742c934add716ed6f
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-01-24 17:42:43 +0800

    Add files via upload

--- LCSOD_05_OOP_part01/Program.cs
+++ LCSOD_05_OOP_part01/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
+
 namespace LCSOD_05_OOP_part01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Staff s1 = new Staff("SCY", 8);
+            Staff s2 = new Staff("ZL", 12);
+            Staff s3 = new Staff("LL", 12);
+            Staff s4 = new Staff("LM", 8);
+
+            Staff[] arr1 = new Staff[4];
+            arr1 = new Staff[4] { s1, s2, s3, s4 };
+
+            for(int i=0; i<4; ++i)
+            {
+                Console.WriteLine(arr1[i].ToString());
+            }
         }
     }
 }
---
+++ LCSOD_05_OOP_part01/staff.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class Staff
+{
+	// data members
+	private string m_name;
+	private const int m_hourlyRate = 30;
+	private int m_hWorked;
+
+	// constructors
+	public Staff(string name, int workHour)
+	{
+		m_name = name;
+		m_hWorked = workHour;
+	}
+	
+	public Staff(string firstName, string lastName, int workHour)
+    {
+		m_name = $"{firstName} {lastName}";
+		m_hWorked = workHour;
+    }
+
+	// getter; setter;
+	public int HourWorked
+	{ get; set; }
+	
+	public int Name
+	{ get; set; }
+
+	// display
+	public void printMessage()
+    {
+		Console.WriteLine("Calculating Pay...");
+    }
+
+	public int calculatePay()
+    {
+		printMessage();
+		return m_hWorked > 0 ? (m_hourlyRate * m_hWorked) : 0;
+    }
+	
+	// overloaing
+	public int calculatePay(int bonus, int allowamce)
+    {
+		printMessage();
+		return m_hWorked > 0 ? (m_hourlyRate * m_hWorked + bonus + allowamce) : 0; 
+    }
+
+
+	// repr
+	public override string ToString()
+    {
+		return ($"name of staff = {m_name}, hourlyRate = {m_hourlyRate}, hourWorked = {m_hWorked}.");
+    }
+}
