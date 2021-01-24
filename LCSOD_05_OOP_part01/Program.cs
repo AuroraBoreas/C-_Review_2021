b3da22f62f2c371d4e3cff1fac67c15b5b41e23d
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-01-24 17:27:27 +0800

    Add files via upload

---
+++ LCSOD_05_OOP_part01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LCSOD_05_OOP_part01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
