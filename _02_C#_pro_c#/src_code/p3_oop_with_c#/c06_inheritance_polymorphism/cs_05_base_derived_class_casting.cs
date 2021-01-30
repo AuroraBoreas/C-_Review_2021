d267b4a8559c648ce938c0c33adeab3b67ef5548
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-01-30 16:53:44 +0800

    Add files via upload

--- _02_C#_pro_c#/src_code/p3_oop_with_c#/c06_inheritance_polymorphism/cs_05_base_derived_class_casting.cs
+++ _02_C#_pro_c#/src_code/p3_oop_with_c#/c06_inheritance_polymorphism/cs_05_base_derived_class_casting.cs
@@ -76,6 +76,7 @@ namespace Casting_Rule
                 
                 + concept
                     - same concept in C++
+                    - core: uniformation or compatibility 
                 
                 + uniqueness
                     - the ultimate base classi in the System is System.Object; everything in c# "is-a" Object
@@ -94,34 +95,179 @@ namespace Casting_Rule
 
             }
 
-            // as kw
+            // kw as
             {
+                /*
+                
+                + casting is not always safe
+                    - explicit casting is evaluated at runtime
+                    - as a programmer, u MUST catch possible invalid casting during runtime
+                
+                + how to do it?
+                    - using try... catch... brainlessly
+                    - using typeof to check type beforehand
+                    - using kw as
+                        - syntax: `var as T;`
+                        - return_value is null or value;
+                    - using kw is
+                        - syntax: `var is T;`
+                        - return false or true;
+                
+                */ 
+                CatchPossibleInvalidCast_trycatch();
+                CatchPossibleInvalidCast_as();
 
             }
 
-            static void GivePromtion(Employee emp) 
+            // kw is can do more, P303
             {
-                System.Console.WriteLine("=> {0} was promoted", emp.Name);
+                // is can also be used in conjunction with discard var _(underscore symbol)
+                // purpose: create a checkall in `if` or `switch` statement
+                
+                object obj = null;
+                if(obj is _)
+                {
+                    // ...;
+                }
+
             }
 
-            static void CastingExample()
+            // pattern matching revisit
             {
-                object zl = new Manager("ZL", 123, 99.99, "123456");
-                GivePromtion(zl); // not OK. compiler doesn't know how to convert object -> Employee
-                GivePromtion((Manager)zl); // OK. explicitly convert object -> Manager;
 
-                Employee xy = new Manager("xy", 345, 99.99, "12345567");
-                GivePromtion(xy); // OK
 
-                SalesPerson zz = new SalesPerson("zz", 567, 9.9, "1234568");
-                GivePromtion(zz); // OK, upcasting
-
-                Console.ReadLine();
             }
+
             
 
 
             return 0;
+        }
+
+        static void GivePromtion(Employee emp) 
+        {
+            System.Console.WriteLine("=> {0} was promoted", emp.Name);
+        }
+
+        static void GivePromtion_is(Employee emp) 
+        {
+            System.Console.WriteLine("=> {0} was promoted", emp.Name);
+            if(emp is SalesPerson)
+            {
+                // ...;
+            }
+            if(emp is Manager)
+            {
+                // ...;
+            }
+        }
+
+        static void GivePromtion_wo_pattern_match(Employee emp)
+        {
+            System.Console.WriteLine("=> {0} was promoted", emp.Name);
+
+            switch(emp)
+            {
+                case SalesPerson s:
+                    // ...;
+                    break;
+                case Manager m:
+                    // ...;
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+        static void GivePromtion_with_pattern_match(Employee emp)
+        {
+            System.Console.WriteLine("=> {0} was promoted", emp.Name);
+
+            switch(emp)
+            {
+                case SalesPerson s when s.SalesNumber > 5:
+                    // ...;
+                    break;
+                case Manager m when m.StockOptions > 10:
+                    // ...;
+                    break;
+            }
+            Console.ReadLine();
+        }
+        static void GivePromtion_with_pattern_match_and_discards(Employee emp)
+        {
+            System.Console.WriteLine("=> {0} was promoted", emp.Name);
+
+            switch(emp)
+            {
+                case SalesPerson s when s.SalesNumber > 5:
+                    // ...;
+                    break;
+                case Manager m when m.StockOptions > 10:
+                    // ...;
+                    break;
+                case Intern _:
+                    // ...;
+                    break;
+                case null:
+                    // ...;
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+        static void CastingExample()
+        {
+            object zl = new Manager("ZL", 123, 99.99, "123456");
+            GivePromtion(zl); // not OK. compiler doesn't know how to convert object -> Employee
+            GivePromtion((Manager)zl); // OK. explicitly convert object -> Manager;
+
+            Employee xy = new Manager("xy", 345, 99.99, "12345567");
+            GivePromtion(xy); // OK
+
+            SalesPerson zz = new SalesPerson("zz", 567, 9.9, "1234568");
+            GivePromtion(zz); // OK, upcasting
+
+            Console.ReadLine();
+        }
+
+        static void CatchPossibleInvalidCast_trycatch()
+        {
+            object zl = new Manager();
+            Hexagon hx;
+
+            try
+            {
+                hex = (Hexagon)zl;
+            }
+            catch(InvalidCastException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+            
+        }
+
+        static void CatchPossibleInvalidCast_as()
+        {
+            object[] arrObjs = new object[4];
+            arrObjs[0] = new Hexagon();
+            arrObjs[1] = false;
+            arrObjs[2] = new Manager();
+            arrObjs[3] = "Last thing";
+            
+            Hexagon hx;
+            foreach(object obj in arrObjs)
+            {
+                hx = obj as Hexagon;
+                if(hx == null)
+                {
+                    System.Console.WriteLine("item is not a hexgon");
+                }
+                else
+                {
+                    hx.Draw();
+                }
+            }
         }
     }
 }
---
+++ _02_C#_pro_c#/src_code/p3_oop_with_c#/c06_inheritance_polymorphism/cs_06_master_parent_class_SystemObject.cs
@@ -0,0 +1,71 @@
+using System;
+
+namespace Master_Parent
+{
+
+    class Person{};
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // core membes of System.Object, P306
+            {
+                /*
+                
+                + core member list
+                    instance method of Object class     meaning in life
+                    - Equals()                          by default, return true only if reference comparison is same in memory;
+                                                        this reference comparison is depended on Hashtable types, using GetHashCode() method;
+                                                        overriding Equals() requires overriding GetHashCode() too 
+                    - Finalize()
+                    - GetHashCode()
+                    - ToString()
+                    - GetType()
+                    - MemberwiseClone()
+
+                + always remember: user-defined types extend Object
+                + reference type in C#
+                    - string
+                    - object
+                    - dynamic
+                
+                + kw to declare a reference type in C#
+                    - record
+                    - class
+                    - dynamic
+                    - delegate
+
+                */ 
+
+            }
+
+            return 0;
+        }
+
+        static void FuckWithSystemObject()
+        {
+            System.Console.WriteLine("=> fuck with System.Object");
+
+            Person p1 = new Person();
+
+            System.Console.WriteLine("ToString: {0}", p1.ToString());
+            System.Console.WriteLine("GetHashCode: {0}", p1.GetHashCode());
+            System.Console.WriteLine("GetType: {0}", p1.GetType());
+
+            // make some other references to p1;
+            // reference types in C# are string, object, dynamic
+            Person p2 = p1;
+            object o = p2;
+
+            // are the referens pointing to the same object in memory?
+            if(o.Equals(p1) && p2.Equals(o))
+            {
+                System.Console.WriteLine("Same Instance!");
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
