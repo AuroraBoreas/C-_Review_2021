7036027b6e47982a1d02c559591d955ee1f7142b
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-01-31 12:35:36 +0800

    Add files via upload

--- _02_C#_pro_c#/src_code/p3_oop_with_c#/c07_structured_exception_handling/cs_06_process_multiple_exceptions.cs
+++ _02_C#_pro_c#/src_code/p3_oop_with_c#/c07_structured_exception_handling/cs_06_process_multiple_exceptions.cs
@@ -6,19 +6,19 @@ namespace Multiple_Exceptions
     {
         public void TurnOn(bool on)
         {
-            System.Console.WriteLine(on? "Jamming...": "Quiet time...");
+            System.Console.WriteLine(on ? "Jamming..." : "Quiet time...");
         }
     }
 
-    class CarIsDeadException: ApplicationException  // version 2
+    class CarIsDeadException : ApplicationException  // version 2
     {
         // private string messageDetails = string.Empty;
-        public DateTime ErrorTimeStamp{get; set;}
+        public DateTime ErrorTimeStamp { get; set; }
         public string CauseOfError { get; set; }
-        
-        public CarIsDeadException(){}
-        public CarIsDeadException(string msg): base(msg){}
-        public CarIsDeadException(string msg, System.Exception inner): base(msg, inner){}
+
+        public CarIsDeadException() { }
+        public CarIsDeadException(string msg) : base(msg) { }
+        public CarIsDeadException(string msg, System.Exception inner) : base(msg, inner) { }
 
 
         public CarIsDeadException(string msg, string cause, DateTime time)
@@ -32,11 +32,11 @@ namespace Multiple_Exceptions
         protected CarIsDeadException(
             System.Runtime.Serialization.SerializationInfo info,
             System.Runtime.Serialization.StreamingContext context)
-        : base(info, context){}
+        : base(info, context) { }
 
         // override Exception.Message props
         // public override string Message => $"Car Error Message: {messageDetails}";
-        
+
     }
 
     class Car   // version 4
@@ -46,10 +46,10 @@ namespace Multiple_Exceptions
         public string PetName { get; set; } = "";
         private bool carIsDead;
         private Radio theMusicBox = new Radio();
-        public Car(){}
+        public Car() { }
         public Car(string name, int speed)
-        { 
-            CurrentSpeed = speed; 
+        {
+            CurrentSpeed = speed;
             PetName = name;
         }
 
@@ -61,15 +61,15 @@ namespace Multiple_Exceptions
         public void Accelerate(int delta)
         {
             // test arg before processing
-            if(delta < 0)
+            if (delta < 0)
             { throw new ArgumentException("delta", "Speed MUST be > 0!"); }
 
-            if(carIsDead)
+            if (carIsDead)
             { System.Console.WriteLine($"{PetName} is out of order.."); }
             else
             {
                 CurrentSpeed += delta;
-                if(CurrentSpeed >= MaxSpeed)
+                if (CurrentSpeed >= MaxSpeed)
                 {
                     // System.Console.WriteLine($"{PetName} has overheated!");
                     CurrentSpeed = 0;
@@ -107,7 +107,7 @@ namespace Multiple_Exceptions
                       just like: .oO0O
 
                 
-                */ 
+                */
                 HandleMultipleException();
             }
 
@@ -125,7 +125,36 @@ namespace Multiple_Exceptions
 
             // inner Exception
             {
+                /*
+                
+                pre-condition: when u encounter an exception while processing another exception
+                
+                + bast practice
+                    - u should record the new exception obj as an "inner exception" wthin a new obj of the same type as the initial exception;
+                
+                + reason
+                    - u need to allocate a new obj of the exception bing handled is that the only way to document an inner exception is via a constructor parameter;
+                
+                */
+                HandleMultipleException4_inner_exception();
+                HandleMultipleException5_inner_exception();
+            }
 
+            // finally block
+            {
+                // same concept in Python
+                HandleMultipleException5_finally_block();
+            }
+
+            // exception filter, when kw
+            {
+                /*
+
+                + mechanism
+                    - when keyword gives u the ability to ensure that the statements within a catch block are executed only if some condition in your code holds true;
+                
+                */
+                HandleMultipleException6_when_kw();
             }
 
             return 0;
@@ -137,7 +166,7 @@ namespace Multiple_Exceptions
 
             Car myCar = new Car("ZL", 20);
             myCar.CrankTunes(true);
-            
+
             try
             {
                 // for(int i=0; i<10; ++i)
@@ -147,16 +176,16 @@ namespace Multiple_Exceptions
                 myCar.Accelerate(-10);
                 Console.ReadLine();
             }
-            catch(ArgumentException ex)
+            catch (ArgumentException ex)
             {
                 System.Console.WriteLine(ex.Message);
             }
             catch (CarIsDeadException ex)
             {
-                 System.Console.WriteLine("\n*** Error! ***");
-                 System.Console.WriteLine($"Message: {ex.Message}\n");
-                 System.Console.WriteLine($"Time: {ex.ErrorTimeStamp}\n");
-                 System.Console.WriteLine($"Cause: {ex.CauseOfError}\n");
+                System.Console.WriteLine("\n*** Error! ***");
+                System.Console.WriteLine($"Message: {ex.Message}\n");
+                System.Console.WriteLine($"Time: {ex.ErrorTimeStamp}\n");
+                System.Console.WriteLine($"Cause: {ex.CauseOfError}\n");
 
                 // display Exception.Data
                 //  System.Console.WriteLine("\n=> custom Data:");
@@ -183,7 +212,7 @@ namespace Multiple_Exceptions
             }
 
             Console.ReadLine();
-            
+
         }
         static void HandleMultipleException3()
         {
@@ -201,7 +230,151 @@ namespace Multiple_Exceptions
             }
 
             Console.ReadLine();
-            
+
+        }
+        static void HandleMultipleException4_inner_exception()
+        {
+            System.Console.WriteLine("=> handling multiple exceptions:");
+            Car myCar = new Car("xy", 90);
+
+            try
+            {
+                myCar.Accelerate(90);
+            }
+            catch (CarIsDeadException e)
+            {
+                // if FileNotFound Error; this statement gonna throw exception
+                System.IO.FileStream fs = System.IO.File.Open(@"C:\carError.txt", System.IO.FileMode.Open);
+            }
+            catch
+            {
+                System.Console.WriteLine("Something wrong happened...");
+                throw;
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static void HandleMultipleException5_inner_exception()
+        {
+            System.Console.WriteLine("=> handling multiple exceptions:");
+            Car myCar = new Car("xy", 90);
+
+            try
+            {
+                myCar.Accelerate(90);
+            }
+            catch (CarIsDeadException e)
+            {
+                try
+                {
+                    // if FileNotFound Error; this statement gonna throw exception
+                    System.IO.FileStream fs = System.IO.File.Open(@"C:\carError.txt", System.IO.FileMode.Open);
+                }
+                catch (Exception e2)
+                {
+                    // throw an exception that records the new exception
+                    // as well as the message of the first exception
+                    throw new CarIsDeadException(e.Message, e2);
+                }
+            }
+            catch
+            {
+                System.Console.WriteLine("Something wrong happened...");
+                throw;
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static void HandleMultipleException5_finally_block()
+        {
+            System.Console.WriteLine("=> handling multiple exceptions:");
+            Car myCar = new Car("xy", 90);
+
+            try
+            {
+                myCar.Accelerate(90);
+            }
+            catch (CarIsDeadException e)
+            {
+                try
+                {
+                    // if FileNotFound Error; this statement gonna throw exception
+                    System.IO.FileStream fs = System.IO.File.Open(@"C:\carError.txt", System.IO.FileMode.Open);
+                }
+                catch (Exception e2)
+                {
+                    // throw an exception that records the new exception
+                    // as well as the message of the first exception
+                    throw new CarIsDeadException(e.Message, e2);
+                }
+            }
+            catch (ArgumentException e)
+            {
+                // ...;
+                throw;
+            }
+            catch
+            {
+                System.Console.WriteLine("Something wrong happened...");
+                throw;
+            }
+            finally
+            {
+                // this will always occur no matter exception or not
+                myCar.CrankTunes(false);
+
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static void HandleMultipleException6_when_kw()
+        {
+            System.Console.WriteLine("=> handling multiple exceptions:");
+            Car myCar = new Car("xy", 90);
+
+            try
+            {
+                myCar.Accelerate(90);
+            }
+            catch (CarIsDeadException e) when (e.ErrorTimeStamp.DayOfWeek != DayOfWeek.Friday)
+            {
+                try
+                {
+                    // if FileNotFound Error; this statement gonna throw exception
+                    System.IO.FileStream fs = System.IO.File.Open(@"C:\carError.txt", System.IO.FileMode.Open);
+                }
+                catch (Exception e2)
+                {
+                    // throw an exception that records the new exception
+                    // as well as the message of the first exception
+                    throw new CarIsDeadException(e.Message, e2);
+                }
+            }
+            catch (ArgumentException e)
+            {
+                // ...;
+                throw;
+            }
+            catch
+            {
+                System.Console.WriteLine("Something wrong happened...");
+                throw;
+            }
+            finally
+            {
+                // this will always occur no matter exception or not
+                myCar.CrankTunes(false);
+
+            }
+
+            Console.ReadLine();
+
         }
 
     }
