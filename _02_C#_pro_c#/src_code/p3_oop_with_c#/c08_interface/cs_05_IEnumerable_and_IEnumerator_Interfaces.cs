99bbea0fbf7dc9304510fc6b8129cee4c9afd11c
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-01-31 18:23:22 +0800

    Add files via upload

--- _02_C#_pro_c#/src_code/p3_oop_with_c#/c08_interface/cs_05_IEnumerable_and_IEnumerator_Interfaces.cs
+++ _02_C#_pro_c#/src_code/p3_oop_with_c#/c08_interface/cs_05_IEnumerable_and_IEnumerator_Interfaces.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 
 namespace Return_Interface
 {
@@ -28,7 +29,120 @@ namespace Return_Interface
     //     }
     // }
 
-    class Garage: System.Collections.IEnumerable    // version 1
+    // class Garage: System.Collections.IEnumerable    // version 1
+    // {
+    //     private Car[] carArray = new Car[4];    // System.Array alrdy implements IEnumerable;
+    //     public Garage()
+    //     {
+    //         carArray[0] = new Car("ZL", 30);
+    //         carArray[1] = new Car("XY", 31);
+    //         carArray[2] = new Car("JJ", 32);
+    //         carArray[3] = new Car("LL", 34);
+    //     }
+
+    //     public System.Collections.IEnumerator GetEnumerator()    // user could find and use GetEnumerator() method
+    //     {
+    //         return carArray.GetEnumerator();
+    //     }
+    // }
+
+    // class Garage: System.Collections.IEnumerable    // version 2
+    // {
+    //     private Car[] carArray = new Car[4];    // System.Array alrdy implements IEnumerable;
+    //     public Garage()
+    //     {
+    //         carArray[0] = new Car("ZL", 30);
+    //         carArray[1] = new Car("XY", 31);
+    //         carArray[2] = new Car("JJ", 32);
+    //         carArray[3] = new Car("LL", 34);
+    //     }
+
+    //     // public System.Collections.IEnumerator GetEnumerator()
+    //     // {
+    //     //     return carArray.GetEnumerator();
+    //     // }
+        
+    //     // hide the functionality of IEnumerable from the obj-lvl; simple make use of explicit interface implementation
+    //     System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    //     {
+    //         return carArray.GetEnumerator();
+    //     }
+    // }
+
+    // class Garage: System.Collections.IEnumerable    // version 3
+    // {
+    //     private Car[] carArray = new Car[4];    // System.Array alrdy implements IEnumerable;
+    //     public Garage()
+    //     {
+    //         carArray[0] = new Car("ZL", 30);
+    //         carArray[1] = new Car("XY", 31);
+    //         carArray[2] = new Car("JJ", 32);
+    //         carArray[3] = new Car("LL", 34);
+    //     }
+
+    //     System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    //     {
+    //         foreach(Car c in carArray)
+    //         { yield return c; }
+    //     }
+
+    //     // foreach is NOT required;
+    //     // but it makes little sense to write it like this anyway; because it wont sync if u update codebase
+    //     // System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    //     // {
+    //     //     yield return carArray[0];
+    //     //     yield return carArray[1];
+    //     //     yield return carArray[2];
+    //     //     yield return carArray[3];
+    //     // }
+        
+    // }
+
+    // class Garage: System.Collections.IEnumerable    // version 4
+    // {
+    //     private Car[] carArray = new Car[4];    // System.Array alrdy implements IEnumerable;
+    //     public Garage()
+    //     {
+    //         carArray[0] = new Car("ZL", 30);
+    //         carArray[1] = new Car("XY", 31);
+    //         carArray[2] = new Car("JJ", 32);
+    //         carArray[3] = new Car("LL", 34);
+    //     }
+
+    //     System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    //     {
+    //         throw new Exception("this won't get called!");
+    //         foreach(Car c in carArray)
+    //         { yield return c; }
+    //     }        
+    // }
+
+    // class Garage: System.Collections.IEnumerable    // version5
+    // {
+    //     private Car[] carArray = new Car[4];    // System.Array alrdy implements IEnumerable;
+    //     public Garage()
+    //     {
+    //         carArray[0] = new Car("ZL", 30);
+    //         carArray[1] = new Car("XY", 31);
+    //         carArray[2] = new Car("JJ", 32);
+    //         carArray[3] = new Car("LL", 34);
+    //     }
+
+    //     System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    //     {
+    //         throw new Exception("this won't get called!");
+    //         return actualImplementation();
+
+    //         // this is private function
+    //         System.Collections.IEnumerator actualImplementation()
+    //         {
+    //             foreach(Car c in carArray)
+    //             { yield return c; }
+
+    //         }
+    //     }        
+    // }
+    class Garage: System.Collections.IEnumerable    // version6
     {
         private Car[] carArray = new Car[4];    // System.Array alrdy implements IEnumerable;
         public Garage()
@@ -39,11 +153,44 @@ namespace Return_Interface
             carArray[3] = new Car("LL", 34);
         }
 
-        public System.Collections.IEnumerator GetEnumerator()
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
-            return carArray.GetEnumerator();
+            // throw new Exception("this won't get called!");
+            return actualImplementation();
+
+            // this is private function
+            System.Collections.IEnumerator actualImplementation()
+            {
+                foreach(Car c in carArray)
+                { yield return c; }
+
+            }
+        }        
+        public IEnumerable GetTheCar(bool returnReversed)
+        {
+            // do some error checking here
+            return actualImplementation();
+
+            IEnumerable actualImplementation()
+            {
+                if(returnReversed)
+                {
+                    for(int i=carArray.Length; i != 0; --i)
+                    {
+                        yield return carArray[i-1];
+                    }
+                }
+                else
+                {
+                    foreach(Car c in carArray)
+                    { yield return c; }
+                }
+                
+            }
+
         }
     }
+
 
     class Program
     {
@@ -92,8 +239,18 @@ namespace Return_Interface
                 FunWithIEnumerableOrEnumerator();   // not OK; user-defined class type does not support GetNumerator() method;
             }
 
-            // IEnumerator
+            // build iterator using yield kw
             {
+                // same concept in Python
+                // do some error checking + using local function as a wrapper to return IEnumerable;
+                FunWithIEnumerableOrEnumerator();
+            }
+
+            // build a named iterator
+            {
+                // just like Python, yield kw can be used witin any method, it creates named iterator
+                // be aware that return_type is IEnumerable interface
+                FunWithYieldKeyword();
 
             }
 
@@ -118,6 +275,28 @@ namespace Return_Interface
 
             // iterate
             foreach(Car c in g)
+            {
+                System.Console.WriteLine("{0} is going {1} MPH", c.Name, c.Speed);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void FunWithYieldKeyword()
+        {
+            System.Console.WriteLine("=> fun with yield keyword:");
+
+            Garage g = new Garage();
+
+            foreach(Car c in g)
+            {
+                System.Console.WriteLine("{0} is going {1} MPH", c.Name, c.Speed);
+            }
+
+            Console.WriteLine();
+
+            // reverse
+            foreach(Car c in g.GetTheCar(true))
             {
                 System.Console.WriteLine("{0} is going {1} MPH", c.Name, c.Speed);
             }
---
+++ _02_C#_pro_c#/src_code/p3_oop_with_c#/c08_interface/cs_06_ICloneable_interface.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Return_Interface
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // ICloneable interface, P367
+            {
+
+            }
+
+            //
+            {
+
+            }
+
+            return 0;
+        }
+
+
+        
+    }
+}
