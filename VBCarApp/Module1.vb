701d945e7f59a95473d125e2feb085ff6d282bf5
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-02-06 09:31:28 +0800

    Add files via upload

---
+++ VBCarApp/Module1.vb
@@ -0,0 +1,28 @@
+﻿Imports CarLibrary
+
+Module Module1
+
+    Public Class SuperCar
+        Inherits SportsCar
+        Public Overrides Sub TurboBoost()
+
+            Console.WriteLine($"in SuperCar, {PetName}, {MaxSpeed}, {CurrentSpeed}")
+
+        End Sub
+
+
+    End Class
+
+    Sub Main()
+        Dim sc As New SuperCar With {
+            .PetName = "ZL",
+            .CurrentSpeed = 15,
+            .MaxSpeed = 100
+        }
+
+        sc.TurboBoost()
+
+        Console.ReadLine()
+    End Sub
+
+End Module
