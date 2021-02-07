d0a4e99084b959fe505376cfb2a49d7ecffb8023
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-02-07 20:02:46 +0800

    Add files via upload

---
+++ _02_C#_pro_c#/src_code/p5_program_with_,NET_assemblies/c16_dynamic_types_and_dynamic_lang_runtime/cs_01_ExportDataToOfficeApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ExportDataToOfficeApp;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExportDataToOfficeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>
+            {
+                new Car {Color="Green", Make="VW", PetName="XY"},
+                new Car {Color="Red", Make="ZL", PetName="DD"},
+                new Car {Color="Black", Make="Ford", PetName="Hank"},
+                new Car {Color="Yellow", Make="BMW", PetName="Davie" }
+            };
+
+            ExportToExcel(cars);
+        }
+
+
+        private static void ExportToExcel(List<Car> carsInstock)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Workbooks.Add();
+
+            Excel._Worksheet ws = excelApp.ActiveSheet;
+
+            ws.Cells[1, "A"] = "Make";
+            ws.Cells[1, "B"] = "Color";
+            ws.Cells[1, "C"] = "Pet Name";
+
+
+            int i = 1;
+            foreach(Car c in carsInstock)
+            {
+                ++i;
+                ws.Cells[i, "A"] = c.Make;
+                ws.Cells[i, "B"] = c.Color;
+                ws.Cells[i, "C"] = c.PetName;
+
+            }
+
+            // format table
+            // nope
+
+            ws.SaveAs($@"{Environment.CurrentDirectory}\Inventory.xlsx");
+            excelApp.Quit();
+            Console.WriteLine("The Inventory.xlsx file has been saved to your app foldr");
+
+        }
+
+
+    }
+}
