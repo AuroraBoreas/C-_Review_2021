5fc26235f6d0e8ee445395cf8043ec7c75dae7a3
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-02-07 15:31:42 +0800

    Add files via upload

---
+++ C#_review_ctor_uniform_initializer/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLibrary
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Person() { }
+        public Person(string firstName)
+        : this(firstName, "noLastName", 1)
+        { }
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName; LastName = lastName; Age = age;
+        }
+        public override string ToString()
+        => $"Name: {FirstName} {LastName}, Age: {Age}";
+    }
+
+}
---
+++ C#_review_ctor_uniform_initializer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PersonLibrary;
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var p = new Person("Zhang", "Liang", 35);   // <-- using parameterized ctors only;
+            Person p4 = new Person { FirstName = "LL", LastName = "MM", Age = 31 }; // <-- using uniform initializer only;
+            Person p2 = new Person() { FirstName="X", LastName="Y", Age=31 };   // <-- using default ctor, and uniform initializer;
+            Person p3 = new Person("DD") { LastName = "EE", Age = 40 }; // <-- using paramaterized ctor, and uniform initializer;
+             
+
+            Console.WriteLine(p.ToString());
+            Console.WriteLine(p2.ToString());
+            Console.WriteLine(p3.ToString());
+            Console.WriteLine(p4.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
