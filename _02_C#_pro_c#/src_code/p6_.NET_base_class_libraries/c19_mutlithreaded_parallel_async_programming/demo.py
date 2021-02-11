caf4118dcd8edcfc23b39110d759f05e8985ff24
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-02-11 21:00:57 +0800

    Add files via upload

--- _02_C#_pro_c#/src_code/p6_.NET_base_class_libraries/c19_mutlithreaded_parallel_async_programming/demo.py
+++ _02_C#_pro_c#/src_code/p6_.NET_base_class_libraries/c19_mutlithreaded_parallel_async_programming/demo.py
@@ -9,9 +9,10 @@ this module demonstrates how to solve 汪一诺 's questions
 被除数 = 698
 除数   = "{}8"
 
-res = []
+res = {}
+print("{}/{} : ".format(被除数, 除数))
 for i in range(1, 10):
     n = 除数.format(i)
     q = 被除数 // int(n)
-    res.append(q)
-    print("{}/{} = {}, 商有{}位".format(被除数, n, q, len(str(q))))
+    print("{}/{} = {:2}, 商有{}位".format(被除数, n, q, len(str(q))))
+print()
