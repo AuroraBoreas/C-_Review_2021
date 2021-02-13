2269bf76ca16a3efecb28e8ef39a657153f77140
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-02-13 10:58:21 +0800

    Add files via upload

--- _02_C#_pro_c#/src_code/p6_.NET_base_class_libraries/c20_fileIO_and_object_serialization/cs_02_Abstract_Stream_Class.cs
+++ _02_C#_pro_c#/src_code/p6_.NET_base_class_libraries/c20_fileIO_and_object_serialization/cs_02_Abstract_Stream_Class.cs
@@ -50,12 +50,75 @@ namespace AbstractStreamClass
                     - WriteAsync()
 
 
+                + note
+                >> the major downfall of working directly with the FileStream type: it demands to operate o raw bytes;
+
                 */
             }
 
-            //
+            // StreamWriter, StreamReader
             {
+                /*
 
+                + when to use:
+                StreamWrite and StreamReader classes are useful whenever u need to read or write character-based data(e.g., string);
+
+                + note:
+                both of them work by default with Unicode characters; u may change it;
+
+
+                */
+                FuckWithFileStreams();
+
+            }
+
+            // Reading from a Text file
+            {
+                /*
+
+                + TextReader core members
+                    - Peak()
+
+                    - Read()
+                    - ReadAsync()
+
+                    - ReadBlock()
+                    - ReadBlockAsync()
+
+                    - ReadLine()
+                    - ReadLineAsync()
+
+                    - ReadToEnd()
+                    - ReadToEndAsync()
+
+
+
+                */
+                // using File.Open()
+                FuckWithStreamWriter();
+                // using File.Open()
+                FuckWithStreamWriter2();
+                // using Directly creating StreamWriter/StreamReader types
+                DirectlyCreatingStreamWriter_StreamReader();
+
+                // confusing?
+                /*
+
+                + keep in mind that the end result is greater flexibility;
+
+                */
+
+            }
+
+            // StringWriter, StringReader
+            {
+                /*
+
+                + why
+                >> u can use StringWriter and StringReader types to treat textual info as a stream of in-memory characters;
+                >> this can prove helpful when u would like to append character-based info to an underlying buffer;
+
+                */
             }
 
             return 0;
@@ -90,6 +153,52 @@ namespace AbstractStreamClass
 
         }
 
+        static void FuckWithStreamWriter()
+        {
+            System.Console.WriteLine("***** Fun with StreamWrite / StreamReader *****\n");
+
+            using(StreamWriter sw = File.CreateText("reminders.txt"))
+            {
+                sw.WriteLine("Dont forget Mother's Day this year...");
+                sw.WriteLine("Dont forget Father's Day this year...");
+                sw.WriteLine("Dont forget these numbers: ");
+
+                for(int i=0; i<10; ++i)
+                    sw.WriteLine(i + " ");
+
+                sw.WriteLine(sw.NewLine);
+            }
+            System.Console.WriteLine("Created file and wrote some thoughts....");
+            Console.ReadLine();
+        }
+
+        static void FuckWithStreamWriter2()
+        {
+            System.Console.WriteLine("***** fun with StreamWrite / StreamReader *****\n");
+
+            System.Console.WriteLine("here are your thoughts: \n");
+            using(StreamReader sr = File.OpenText("reminders.txt"))
+            {
+                string input = null;
+                while((input = sr.ReadLine()) != null)
+                {
+                    System.Console.WriteLine(input);
+                }
+            }
+            Console.ReadLine();
+
+        }
+
+        static void DirectlyCreatingStreamWriter_StreamReader()
+        {
+            System.Console.WriteLine("***** fun with StreamWriter / StreamReader *****\n");
+
+            using(StreamWriter sw = new StreamWriter(@"reminders.txt"))
+            {}
+
+            using(StreamReader sr = new StreamReader(@"reminders.txt"))
+            {}
+        }
 
     }
 }
