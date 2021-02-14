da0c0483213c28bf1195f8ca759d0586efc0c770
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-02-14 15:29:14 +0800

    Add files via upload

---
+++ _02_C#_pro_c#/src_code/p7_windows_presentation_foundation/c24_WPF_and_XAML/cs_01_WPF_XAML.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace WPF_XAML
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // P1011
+            {
+                /*
+
+                + definition
+                    - XAML: extensible application markup language
+
+                + why
+                    - Windows Forms is rather asymmetrical
+                    >> System.Windows.Forms.dll and System.Drawing.dll do NOT provide direct support for many additional technologies require to build a feature-rich desktop application;
+
+
+                + pre-WPF solutions to desired functionalities
+
+                    | desired functionality                 | Technology                             |
+                    |---------------------------------------|----------------------------------------|
+                    | build windows with controls           | Windows Forms                          |
+                    | 2D graphics support                   | GDI+(System.Drawing.dll)               |
+                    | 3D graphics support                   | DirectX APIs                           |
+                    | support for streaming video           | Windows Media Player APIs              |
+                    | support for flow-style documents      | Programmatic manipulation of PDF files |
+
+
+                    >> demrit
+                        >> each technology requires a radically different mind-set;
+                        >> it is difficult for a Wondows Forms programmer to master the diverse nature of each API;
+                */
+
+            }
+
+            //
+            {
+
+            }
+
+            return 0;
+        }
+
+
+
+    }
+}
---
+++ _02_C#_pro_c#/src_code/p7_windows_presentation_foundation/c27_WPF_resources_animation_styles_and_templates/template.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace XXXXX
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            //
+            {
+
+            }
+
+            //
+            {
+
+            }
+
+            return 0;
+        }
+
+
+
+    }
+}
