1cb7a16d83ea5050496481f9c55ff9cb384cabd1
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-02-14 11:03:07 +0800

    Add files via upload

--- _02_C#_pro_c#/src_code/p6_.NET_base_class_libraries/c22_entity_framework6/cs_01_role_of_entity_framework.cs
+++ _02_C#_pro_c#/src_code/p6_.NET_base_class_libraries/c22_entity_framework6/cs_01_role_of_entity_framework.cs
@@ -6,13 +6,28 @@ namespace RoleOfEntityFramework6
     {
         static int Main(string[] args)
         {
-            //
+            // EF
             {
+                /*
+
+                + what
+                    - a new component of the ADO.NET API called the "Entity Framework"(or simply, EF);
+
+                    - conceptually, rather than treating a batch of data as a colletion of rows and columns, u can operate on a collection of strongly typed objects termed "Entity";
+
+                + why
+                    - "Entity" are natively LINQ aware
+                    - u can query against them using the same LINQ grammar
+                    - EF runtime engine translates your LINQ queries into proper SQL queries;
+
+                */
 
             }
 
-            //
+            // P905 - P953
             {
+
+                // need AutLot database, skip
 
             }
 
