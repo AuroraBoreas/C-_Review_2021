c83c8494581999b301863adfa28db3222cc07824
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-02-14 15:05:18 +0800

    Add files via upload

---
+++ _02_C#_pro_c#/src_code/p6_.NET_base_class_libraries/c23_windows_communication_foundation/cs_01_windows_communication_foundation.cs
@@ -0,0 +1,70 @@
+using System;
+
+namespace WindowsCommunicationFoundation
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // P954
+            {
+                /*
+
+                + what
+                    - WCF is the name of the API designed specifically for the process of building distributed system;
+
+                + why
+                    - unlike specific distributed APIs, u might have used in the past(DCOM, .NET remoting, XML web services, message queueing), WCF provides a single, unified, and extendable programming object MODEL that u an use to interact with a number of previously diverse distributed technologies;
+
+
+                + aspects of WCF
+                    - interoperability
+                    - integration
+
+                    - support for strongly typed as well as untyped messages;
+
+                    - support for several bindings(e.g., raw HTTP, TCP, MSMQ, WebSockets, named pipes) allows u to choose the most appropriate plumbing to transport message data;
+
+                    - support for the latest-and-greatest web service specifications
+
+                    - a fully integrated security model encompassing both native windows/.NET security protocols and numerous neutral-security techniques built on web service standards;
+
+                    - support for session-like state management techniques, as well as support for one-way or stateless message;
+
+                */
+
+            }
+
+            // basic composition of a WCF application
+            {
+                /*
+
+                + WCF service assembly
+
+                + WCF service host
+
+                + WCF client
+
+                */
+
+            }
+
+            // ABCs of WCF
+            {
+                /*
+
+                + what is ABCs
+                    - Address
+                    - Binding
+                    - Contract
+
+                */
+            }
+
+            return 0;
+        }
+
+
+
+    }
+}
